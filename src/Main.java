9c544537063514f7f987e3060e6d69c0cfcc6d22
Federico Heinrich <fedeheinrich199@gmail.com>
2025-08-06 21:04:29 -0300

    Subiendo proyecto a Github

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class Main{
+    public static void main(String[] args) {
+        // Crea una instancia de Gson
+        Gson gson = new Gson();
+    }
+}
+
